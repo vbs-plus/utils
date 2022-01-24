6f4bef20dad537a0352ac5631368b1ef1d822e9d
WuHao <2323333339@qq.com>
2022-01-24 17:25:35 +0800

    fix: 更新

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,6 @@
 import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
-import typescript from 'rollup-plugin-typescript';
+import typescript from 'rollup-plugin-typescript2';
 import { terser } from 'rollup-plugin-terser';
 
 // 插件列表
@@ -12,13 +12,15 @@ if (process.env.NODE_ENV === 'production') plugins.push(terser());
 export default [
   {
     input: 'src/index.ts',
-    output: {
-      // 将插件挂载到 window 上: window.VUtils
-      name: 'VUtils',
-      file: 'dist/index.js',
-      format: 'umd',
-      exports: 'auto'
-    },
+    output: [
+      {
+        // 将插件挂载到 window 上: window.VUtils
+        file: 'dist/index.js',
+        format: 'umd',
+        name: 'VUtils',
+        inlineDynamicImports: true,
+      }
+    ],
     plugins
   },
 ]
