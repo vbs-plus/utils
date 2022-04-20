8a80fd60a4d101402adfa8587e8ce5ca0074fee9
WuHao <2323333339@qq.com>
2022-04-20 17:14:57 +0800

    feat: Update function name

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,11 +1,11 @@
 import getType from "./type/getType";
 import isQQ from "./validate/isQQ";
 import isPhone from "./validate/isPhone";
-import isCard from "./validate/isCard";
+import isIDCard from "./validate/isIDCard";
 import isEmail from "./validate/isEmail";
 import isBankCard from "./validate/isBankCard";
 import compressImage from "./method/compressImage";
 import parseUrl from "./method/parseUrl";
 import dataURLtoBlob from "./method/dataURLtoBlob";
 import dataURLtoFile from "./method/dataURLtoFile";
-export { getType, isQQ, parseUrl, compressImage, isPhone, isCard, isEmail, isBankCard, dataURLtoBlob, dataURLtoFile };
+export { getType, isQQ, parseUrl, compressImage, isPhone, isIDCard, isEmail, isBankCard, dataURLtoBlob, dataURLtoFile };
---
+++ dist/validate/isIDCard.d.ts
@@ -0,0 +1,7 @@
+declare const _default: (data: string) => boolean;
+/**
+ * @description: 校验是否为身份证
+ * @param {string} data
+ * @return {*}
+ */
+export default _default;
